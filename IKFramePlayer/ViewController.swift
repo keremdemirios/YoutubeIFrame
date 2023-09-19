d2a1e41f723282366d7d061455373f3cb400eb5a
Kerem Demir <keremdemir1694@gmail.com>
2023-09-19 19:37:59 +0300

    Swift package manager for youtube

---
+++ IKFramePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IKFramePlayer
+//
+//  Created by Kerem Demir on 19.09.2023.
+//
+
+import UIKit
+//import YouTubeiOSPlayerHelper
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IKFramePlayer/YoutubeIFrameController.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeIFrameController.swift
+//  IKFramePlayer
+//
+//  Created by Kerem Demir on 19.09.2023.
+//
+
+import UIKit
+
+class YoutubeIFrameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
