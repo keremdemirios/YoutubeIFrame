e563acc9b4361e63239f43b64fd489c24386a3b3
Kerem Demir <keremdemir1694@gmail.com>
2023-09-19 19:43:42 +0300

    YPlayerView is integrated.

--- IKFramePlayer/YoutubeIFrameController.swift
+++ IKFramePlayer/YoutubeIFrameController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import YouTubeiOSPlayerHelper
 
 class YoutubeIFrameController: UIViewController {
 
+    @IBOutlet weak var playerView: YTPlayerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.playerView.load(withVideoId: "M7lc1UVf-VE")
         
     }
 }
