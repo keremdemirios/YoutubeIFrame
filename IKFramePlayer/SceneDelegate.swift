90ce5be16108f1e5a31f6a29d78d935b9ad88bbe
Kerem Demir <keremdemir1694@gmail.com>
2023-09-20 14:38:29 +0300

    Removed storyboard. Created programmatically screen.

--- IKFramePlayer/SceneDelegate.swift
+++ IKFramePlayer/SceneDelegate.swift
@@ -13,10 +13,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Create the main window
+        let window = UIWindow(windowScene: windowScene)
+        
+        // Create the main ViewController
+        let viewController = ViewController() // ViewController burada kendi ViewController sınıfınıza göre güncellenmelidir
+        
+        // Create a UINavigationController containing the main ViewController (optional)
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        // Set the root ViewController for the window
+        window.rootViewController = navigationController  // You can use viewController instead of navigationController
+        
+        // Display the window
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ IKFramePlayer/ViewController.swift
@@ -0,0 +1,44 @@
+// TO DO : Youtube linkinin sonundaki kisim ID olarka geciyor.
+// TO DO : Anasayfada bir tane textfield yap ve arama ile video getir. Istege gore farkli sekilde yapabilirsin.
+// TO DO : Video linkini nasil getirecegini bul.
+//  YoutubeIFrameController.swift
+//  IKFramePlayer
+//
+//  Created by Kerem Demir on 19.09.2023.
+//
+
+import UIKit
+import YouTubeiOSPlayerHelper
+
+class ViewController: UIViewController {
+    
+    private let playerView: YTPlayerView = {
+        let playerView = YTPlayerView()
+        playerView.translatesAutoresizingMaskIntoConstraints = false
+        playerView.layer.borderWidth = 1
+        playerView.layer.borderColor = UIColor.label.cgColor
+        playerView.layer.cornerRadius = 8
+        return playerView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configure()
+    }
+    
+    private func configure(){
+        configureUI()
+        self.playerView.load(withVideoId: "jBryHOdyx_k")
+    }
+    
+    private func configureUI(){
+        view.addSubview(playerView)
+        NSLayoutConstraint.activate([
+            playerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            playerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            playerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            playerView.heightAnchor.constraint(equalToConstant: 250)
+        ])
+    }
+}
