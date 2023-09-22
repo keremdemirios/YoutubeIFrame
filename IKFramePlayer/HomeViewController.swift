3d31651c80683b5ba87a7930509712dcf130a2bc
Kerem Demir <keremdemir1694@gmail.com>
2023-09-22 17:14:14 +0300

    Hide keyboard extension, textfield

--- IKFramePlayer/HomeViewController.swift
+++ IKFramePlayer/HomeViewController.swift
@@ -12,7 +12,17 @@ class HomeViewController: UIViewController {
     private let searchTextField: UITextField = {
         let searchTextField = UITextField()
         searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.backgroundColor = .label
+        searchTextField.layer.cornerRadius = 12
+        searchTextField.textColor = .systemBackground
         return searchTextField
+    }()
+    
+    lazy var searchButton:UIButton = {
+        let searchButton = UIButton()
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+        return searchButton
     }()
     
     // MARK : Life Cycle
@@ -23,17 +33,23 @@ class HomeViewController: UIViewController {
     }
     // MARK : Configure
     private func configure(){
+        hideKeyboardWhenTappedAround() // Hide keyboard when tapped around + extension method
         setupUI()
     }
     // MARK : Setup UI
     private func setupUI(){
         view.addSubViews(searchTextField)
         NSLayoutConstraint.activate([
-            
+            searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            searchTextField.widthAnchor.constraint(equalToConstant: 200),
+            searchTextField.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     // MARK : Functions
     
     // MARK : Actions
-
+    @objc func searchButtonTapped(){
+        print("Working")
+    }
 }
